59279db830dc17bd135f2c30ebdcba15d65dde1c
Scarfacehbc <p.jipp87@gmail.com>
2022-06-13 09:58:05 +0200

    ....

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, ImageBackground, StyleSheet, Text, View } from "react-native";
+import { Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground
+        style={styles.logo}
+        source={require("./images/mainImage.jpg")}
+      >
+        <View style={{ flex: 0.3 }}>
+          <Text style={{ fontSize: 36, fontWeight: "bold", color: "white" }}>
+            MyFitnesApp
+          </Text>
+        </View>
+        <Text style={{ color: "white", flex: 0.3 }}>
+          Open up App.js to start working on your app!
+        </Text>
+        <Button
+          title="Start"
+          style={{ flex: 0.3 }}
+          onPress={() => alert("klappt")}
+        />
+      </ImageBackground>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +32,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  logo: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
