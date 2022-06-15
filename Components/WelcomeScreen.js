7ef3d0c9521f1549aecfdc65efb9fe5f885d7982
pjipp <p.jipp87@gmail.com>
2022-06-15 12:16:47 +0200

    ...

--- Components/WelcomeScreen.js
+++ Components/WelcomeScreen.js
@@ -1,13 +1,52 @@
-import { View, Text, ImageBackground, StyleSheet } from "react-native";
-import React from "react";
+import { View, ImageBackground, StyleSheet } from "react-native";
+import { Button, Headline, Text } from "react-native-paper";
+import React, { useRef } from "react";
+import AppLoading from "expo-app-loading";
+import {
+  useFonts,
+  BlackOpsOne_400Regular,
+} from "@expo-google-fonts/black-ops-one";
+import { Animated } from "react-native";
 
 export default function WelcomeScreen() {
+  let [fontsLoaded] = useFonts({
+    BlackOpsOne_400Regular,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  const AnimateColor = () => {
+    Animated.timing(colorAnimation, {
+      toValue: "red",
+      duration: 3000,
+      useNativeDriver: true,
+    }).start();
+  };
+
   return (
     <ImageBackground
       style={styles.background}
       source={require("../images/BackgroundImages/WelcomeScreenBackground.jpg")}
     >
-      <Text style={{ color: "blue" }}>Hallo</Text>
+      <View style={{ flex: 0.2, elevation: 50 }}>
+        <Headline
+          style={{
+            flex: 0.5,
+            justifyContent: "center",
+            textAlignVertical: "center",
+            fontFamily: "BlackOpsOne_400Regular",
+            color: "white",
+            fontSize: 40,
+          }}
+        >
+          Zeit fürs Training
+        </Headline>
+      </View>
+      <View style={{ flex: 0.7 }}>
+        <Button>Text</Button>
+        <Text style={{ color: "blue" }}>Hallo</Text>
+      </View>
     </ImageBackground>
   );
 }
