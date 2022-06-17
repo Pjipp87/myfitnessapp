c14da286e81c7c98ec5b7488d0e7c5c3ba7c4f9f
ScarfaceHBC <p.jipp87@gmail.com>
2022-06-17 08:57:26 +0200

    ...

--- Components/CreateWorkoutScreen.js
+++ Components/CreateWorkoutScreen.js
@@ -1,12 +1,62 @@
-import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import React, { useEffect, useState } from "react";
 import { ImageBackground } from "react-native";
-import { TextInput } from "react-native-paper";
+import { TextInput, Button } from "react-native-paper";
+import {
+  useFonts,
+  BlackOpsOne_400Regular,
+} from "@expo-google-fonts/black-ops-one";
+
+const DATA = [
+  {
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "First Item",
+  },
+  {
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    title: "Second Item",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Third Item",
+  },
+];
+
+const Item = ({ title }) => (
+  <View style={{ paddingVertical: 5 }}>
+    <Text
+      style={{
+        backgroundColor: "white",
+        textAlign: "center",
+        fontFamily: "BlackOpsOne_400Regular",
+        paddingVertical: 10,
+        fontSize: 20,
+      }}
+    >
+      {title}
+    </Text>
+  </View>
+);
 
 export default function CreateWorkoutScreen() {
+  const [newWorkoutName, setNewWorkoutName] = useState("Dein neues Workout");
+
+  let [fontsLoaded] = useFonts({
+    BlackOpsOne_400Regular,
+  });
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  //####################################################################
+
+  const renderItem = ({ item }) => <Item title={item.title} />;
+
+  //#####################################################################
+
   return (
     <ImageBackground
-      source={require("../images/BackgroundImages/CreateWorkoutScreen.jpg")}
+      source={require("../images/BackgroundImages/iron-plate.jpg")}
       style={{ flex: 1 }}
     >
       <View style={{ paddingTop: 20 }}>
@@ -16,9 +66,10 @@ export default function CreateWorkoutScreen() {
             color: "white",
             textAlign: "center",
             elevation: 30,
+            fontFamily: "BlackOpsOne_400Regular",
           }}
         >
-          Dein Workout:
+          Neues Workout:
         </Text>
         <TextInput
           style={styles.textInputField}
@@ -28,6 +79,48 @@ export default function CreateWorkoutScreen() {
           activeOutlineColor="black"
         ></TextInput>
       </View>
+      <Text
+        style={{
+          fontSize: 28,
+          color: "white",
+          textAlign: "center",
+          elevation: 40,
+          fontFamily: "BlackOpsOne_400Regular",
+        }}
+      >
+        Übungsname:
+      </Text>
+      <TextInput
+        style={styles.textInputField}
+        label="Name der Übung"
+        mode={"outlined"}
+        selectionColor="blue"
+        activeOutlineColor="black"
+      />
+
+      <Button
+        mode="contained"
+        labelStyle={{ fontFamily: "BlackOpsOne_400Regular" }}
+      >
+        Hinzufügen
+      </Button>
+      <Text
+        style={{
+          paddingVertical: 30,
+          fontSize: 30,
+          color: "white",
+          textAlign: "center",
+
+          fontFamily: "BlackOpsOne_400Regular",
+        }}
+      >
+        {newWorkoutName}
+      </Text>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
     </ImageBackground>
   );
 }
