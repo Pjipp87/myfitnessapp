4ecc2966176c0ed1e8823f7ad7e4b0f28edc96a2
Scarfacehbc <p.jipp87@gmail.com>
2022-06-21 13:34:31 +0200

    .....

--- Components/SettingsScreen.js
+++ Components/SettingsScreen.js
@@ -1,10 +1,37 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Button } from "react-native-paper";
+
+import { Audio } from "expo-av";
 
 export default function SettingsScreen() {
+  const [sound, setSound] = useState();
+
+  const playSound = async () => {
+    console.log("Loading Sound");
+    const { sound } = await Audio.Sound.createAsync(
+      require("../sounds/mixkit-confirmation-tone-2867.wav")
+    );
+    setSound(sound);
+
+    console.log("Playing Sound");
+    await sound.playAsync();
+  };
+
+  useEffect(() => {
+    return sound
+      ? () => {
+          console.log("Unloading Sound");
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
   return (
     <View>
       <Text>SettingsScreen Test</Text>
+      <Text>TestScreen</Text>
+      <Button onPress={() => playSound()}>Soundtest</Button>
     </View>
   );
 }
