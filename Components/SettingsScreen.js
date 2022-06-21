c967de071f65c860e3916aa8d48a5eaacb644cf6
ScarfaceHBC <p.jipp87@gmail.com>
2022-06-21 21:31:08 +0200

    ....

--- Components/SettingsScreen.js
+++ Components/SettingsScreen.js
@@ -3,7 +3,16 @@ import React, { useState, useEffect } from "react";
 import { Button } from "react-native-paper";
 
 export default function SettingsScreen() {
-  const [counter, setCounter] = useState(10);
+  const [counter, setCounter] = useState(0);
+
+  let countdown = 0;
+  useEffect(() => {
+    setInterval(() => {
+      countdown++;
+      setCounter(countdown);
+      console.log(countdown);
+    }, 500);
+  }, [counter]);
 
   return (
     <View>
@@ -21,9 +30,8 @@ export default function SettingsScreen() {
             <Text>Counter</Text>
             <Text>{counter}</Text>
             <Button onPress={() => startCounter()}>Start</Button>
-            <Button onPress={() => stop()}>Stop</Button>
+            <Button onPress={() => stopTimer()}>Stop</Button>
           </View>
-          <UHR pause={pause}></UHR>
         </View>
       </View>
     </View>
